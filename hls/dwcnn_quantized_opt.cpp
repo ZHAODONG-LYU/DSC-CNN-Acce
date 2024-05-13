8af07efac0d6340fb0d1080641a4d8bc96e9759c
ZHAODONG LYU <103206736+ZHAODONG-LYU@users.noreply.github.com>
2024-05-14 00:14:08 +0800

    Update dwcnn_quantized_opt.cpp
    delete some useless codes and annotations

--- hls/dwcnn_quantized_opt.cpp
+++ hls/dwcnn_quantized_opt.cpp
@@ -6,7 +6,6 @@
 #include <iostream>
 #include "ap_fixed.h"	//ap_fixed<18,6,AP_TRN_ZERO,AP_SAT>		<W,I,Q,O,N>
 #include "ap_int.h"	//ap_int<N> or ap_uint<N>, 1<=N<=1024
-// #include "hls_math.h"	//data_t s = hls::sinf(angle);
 #include "hls_stream.h"
 using namespace hls;
 
