2773b0494eccd1613581e77d15589b4fe4b4ccd2
ZHAODONG LYU <103206736+ZHAODONG-LYU@users.noreply.github.com>
2024-05-14 00:20:44 +0800

    Update dwcnn_quantized_opt.h

--- hls/dwcnn_quantized_opt.h
+++ hls/dwcnn_quantized_opt.h
@@ -5,15 +5,13 @@
 #include <stdlib.h>
 #include "ap_fixed.h"	//ap_fixed<18,6,AP_TRN_ZERO,AP_SAT>		<W,I,Q,O,N>
 #include "ap_int.h"	//ap_int<N> or ap_uint<N>, 1<=N<=1024
-// #include "hls_math.h"	//data_t s = hls::sinf(angle);
 #include "hls_stream.h"
-const int TWFIVEBITS = 25;
-const int TWFOURBITS = 24;
-const int SIXTEENBITS = 16;
+const int TWFIVEBITS = 25; // for debug
+const int TWFOURBITS = 24; // maximum intermediate, accumulation, 24-bits
+const int SIXTEENBITS = 16; 
 const int EIGHTBITS = 8;
-const int SLICE_LEGTH = 1600;
+const int SLICE_LEGTH = 1600; // input size, line format
 
-//ap_int<SIXTEENBITS> dwcnnopt(hls::stream<ap_int<EIGHTBITS>> &datainput);
 void dwcnnopt(hls::stream<ap_int<EIGHTBITS>> &datainput, hls::stream<ap_int<SIXTEENBITS>> &out);
 
 #endif
